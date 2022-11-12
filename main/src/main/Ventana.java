10ec07f371345ee67d14843334162da33752fcc9
v1d3 <ccristobalc132@gmail.com>
2022-11-12 00:14:51 -0300

    Merging

--- main/src/main/Ventana.java
+++ main/src/main/Ventana.java
@@ -8,8 +8,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.ButtonGroup;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
