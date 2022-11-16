a29bf6198c7fdb3a357ad81f1a26cec25e430ce9
v1d3 <ccristobalc132@gmail.com>
2022-11-15 22:38:29 -0300

    Merging with repository

--- main/src/main/Ventana.java
+++ main/src/main/Ventana.java
@@ -9,7 +9,9 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 
@@ -19,21 +21,24 @@ class Ventana extends JFrame {
 
     private PanelPrincipal dp;
     private PanelesPerifericos PP;
+    //private JLabel JL = new JLabel();
 
     Ventana() {
         this.setLayout(new BorderLayout());
         dp = new PanelPrincipal();
         PP = new PanelesPerifericos();
         dp.setLayout(null);
-
+        //JL.setIcon(new ImageIcon("C:\\Users\\ccris\\OneDrive\\Escritorio\\pictures-steam profile\\dispensador.jpg"));
         PP.getBoton().ActivateActionListener();
         PP.getBoton().ButtonGroup();    //Activa el boton comprame
-        PP.addButtonsCoordinate();
-
+        PP.addButtonsCoordinate();      //Agrega coordenadas a los botones
+        //JL.setBounds(150, 50, 1000, 650);
+        
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//activar cierre en la X 
         this.add(dp);
-        PP.addBotonestoPanel(dp);
-        this.setSize(1000, 800); //se pueden pasar como parámetros del constructor
+        PP.addBotonestoPanel(dp);   //Añade los botones al panel Principal
+        //dp.add(JL);
+        this.setSize(1000, 800); //Tamaño de la ventana
         this.setVisible(true);  //para que se abra y sea visible 
         setLocationRelativeTo(null);
     }
@@ -49,12 +54,15 @@ class PanelPrincipal extends JPanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+        //Color c = new Color(156, 32, 5);        
+        //g.setColor(c);
+        //g.fillRect(40, 40, 495, 660); //51,51 501, 651
         g.setColor(Color.red);
         g.fillRect(50, 50, 500, 650); //Rectangulo y dimensiones
 
         g.setColor(Color.gray);
         g.fillRect(80, 80, 440, 550); //Rectangulo y dimensiones
-        g.setColor(Color.black);     //Rectangulo donde se situara el dinero
+        g.setColor(Color.LIGHT_GRAY);     //Rectangulo donde se situara el dinero
         g.fillRect(380, 550, 170, 150);
 
     }
@@ -140,11 +148,11 @@ class Botones extends JButton implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (Rb1.isSelected()) {
-            JOptionPane.showMessageDialog(this, "Asi que te gusta la fanta eh?.");
+            JOptionPane.showMessageDialog(this, "Asi que te gusta la PIJA eh?.");
         } else if (Rb2.isSelected()) {
-            JOptionPane.showMessageDialog(this, "Asi que te gusta el Sprite eh?.");
+            JOptionPane.showMessageDialog(this, "GEY?.");
         } else if (Rb3.isSelected()) {
-            JOptionPane.showMessageDialog(this, "wacala");
+            JOptionPane.showMessageDialog(this, "Te gusta la pija negra");
         } else if (!(Rb1.isSelected() && Rb2.isSelected() && Rb3.isSelected())) {   //En caso de no haber elegido ningun tipo de Bebida
             JOptionPane.showMessageDialog(this, "No has seleccionado ninguna Bebida");
         }
